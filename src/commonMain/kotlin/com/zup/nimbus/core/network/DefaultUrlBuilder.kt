d4395460e518604f27bfb015beb5328969d5369c
Arthur Bleil <78227085+arthurbleilzup@users.noreply.github.com>
2022-04-14 17:37:57 -0300

    feat: url builder (#4)
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- src/commonMain/kotlin/com/zup/nimbus/core/network/DefaultUrlBuilder.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/network/DefaultUrlBuilder.kt
@@ -1,4 +1,13 @@
 package com.zup.nimbus.core.network
 
-class DefaultUrlBuilder: UrlBuilder {
+import com.zup.nimbus.core.utils.UriUtils
+import com.zup.nimbus.core.utils.removeSuffix
+import com.zup.nimbus.core.utils.then
+
+class DefaultUrlBuilder(private val baseUrl: String) : UrlBuilder {
+  override fun build(path: String): String {
+    val base = removeSuffix(baseUrl, "/")
+    val url = path.matches(Regex("^\\/+(\\b|$)")) then "${base}${path}" ?: path
+    return (UriUtils.decode(url) == url) then UriUtils.encode(url) ?: url
+  }
 }
---
+++ src/commonMain/kotlin/com/zup/nimbus/core/utils/string.kt
@@ -0,0 +1,45 @@
+package com.zup.nimbus.core.utils
+
+/**
+ * Removes a prefix from the string.
+ *
+ * @param str the string to have the `prefix` removed from
+ * @param prefix the prefix to remove from the string
+ * @returns the string without the prefix
+ */
+fun removePrefix(str: String, prefix: String): String {
+  return str.replace(Regex("^${prefix}"), "")
+}
+
+/**
+ * Adds a single character to start of the string if the string doesn't yet start with this
+ * character.
+ *
+ * @param str the string to add the prefix character to
+ * @param prefix the single character to add as prefix
+ * @returns the resulting string
+ */
+fun addPrefix(str: String, prefix: String): String {
+  return (str.isEmpty() || str[0].toString() != prefix) then "${prefix}${str}" ?: str
+}
+
+/**
+ * Removes a suffix from the string.
+ *
+ * @param str the string to have the `suffix` removed from
+ * @param suffix the suffix to remove from the string
+ * @returns the string without the suffix
+ */
+fun removeSuffix(str: String, suffix: String): String {
+  return str.replace(Regex("${suffix}$"), "")
+}
+
+/**
+ * Transforms the first letter of the string into an uppercase letter.
+ *
+ * @param str the string to capitalize
+ * @returns the resulting string
+ */
+fun capitalizeFirstLetter(str: String): String {
+  return  (str.isEmpty()) then str ?: "${str[0].toString().uppercase()}${str.slice((1 until (str.length - 1)))}"
+}
