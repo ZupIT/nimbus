14ee4422d5ca319cfca68dce515469736a9c2aa2
Arthur Bleil <arthur.bleil@zup.com.br>
2022-04-14 14:17:20 -0300

    feature: default logger
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- src/commonMain/kotlin/com/zup/nimbus/core/log/DefaultLogger.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/log/DefaultLogger.kt
@@ -1,4 +1,60 @@
 package com.zup.nimbus.core.log
 
 class DefaultLogger: Logger {
+  private var isEnabled = true
+
+  private enum class LoggerColor(val code: String) {
+    RESET("\u001B[0m"),
+    BLUE("\u001B[34m"),
+    CYAN("\u001B[36m"),
+    GREEN("\u001B[32m"),
+    PURPLE("\u001B[35m"),
+    RED("\u001B[31m"),
+    WHITE("\u001B[37m"),
+    YELLOW("\u001B[33m"),
+  }
+
+  override fun enable() {
+    isEnabled = true
+  }
+
+  override fun disable() {
+    isEnabled = false
+  }
+
+  override fun log(vararg messages: String) {
+    print(LoggerColor.WHITE, *messages)
+  }
+
+  override fun info(vararg messages: String) {
+    print(LoggerColor.BLUE, *messages)
+  }
+
+  override fun warn(vararg messages: String) {
+    print(LoggerColor.YELLOW, *messages)
+  }
+
+  override fun success(vararg messages: String) {
+    print(LoggerColor.GREEN, *messages)
+  }
+
+  override fun error(vararg messages: String) {
+    print(LoggerColor.RED, *messages)
+  }
+
+  override fun lifecycle(vararg messages: String) {
+    print(LoggerColor.PURPLE, *messages)
+  }
+
+  override fun expression(vararg messages: String) {
+    print(LoggerColor.CYAN, *messages)
+  }
+
+  private fun print(color: LoggerColor, vararg messages: String) {
+    if (isEnabled) {
+      messages.forEach {
+        println("${color.code}${it}${LoggerColor.RESET.code}")
+      }
+    }
+  }
 }
--- src/commonMain/kotlin/com/zup/nimbus/core/log/Logger.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/log/Logger.kt
@@ -1,4 +1,13 @@
 package com.zup.nimbus.core.log
 
 interface Logger {
+  fun enable()
+  fun disable()
+  fun log(vararg messages: String)
+  fun info(vararg messages: String)
+  fun warn(vararg messages: String)
+  fun error(vararg messages: String)
+  fun success(vararg messages: String)
+  fun lifecycle(vararg messages: String)
+  fun expression(vararg messages: String)
 }
