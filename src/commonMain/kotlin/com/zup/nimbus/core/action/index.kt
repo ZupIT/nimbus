68ae68b1624647e09e76d7b71d2920f0e561b446
Tiago Peres França <tiago.franca@zup.com.br>
2022-05-04 17:46:36 -0300

    creates remaining navigation actions

--- src/commonMain/kotlin/com/zup/nimbus/core/action/index.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/action/index.kt
@@ -3,6 +3,9 @@ package com.zup.nimbus.core.action
 import com.zup.nimbus.core.ActionHandler
 
 val coreActions = mapOf<String, ActionHandler>(
-  "pushView" to { pushView(it) },
-  "popView" to { popView(it) },
+  "push" to { push(it) },
+  "pop" to { pop(it) },
+  "popTo" to { popTo(it) },
+  "present" to { present(it) },
+  "dismiss" to { dismiss(it) },
 )
