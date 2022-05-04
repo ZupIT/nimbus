0d681bbd50a0f52385ebd785a558e0a54b82e984
Tiago Peres França <tiago.franca@zup.com.br>
2022-05-04 09:03:13 -0300

    removed url encoding from the code

--- src/commonMain/kotlin/com/zup/nimbus/core/network/DefaultUrlBuilder.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/network/DefaultUrlBuilder.kt
@@ -2,7 +2,13 @@ package com.zup.nimbus.core.network
 
 import com.zup.nimbus.core.utils.removeSuffix
 
-class DefaultUrlBuilder(private val baseUrl: String) : UrlBuilder {
+class DefaultUrlBuilder(
+  /**
+   * The baseUrl to use when building the URLs. If the baseUrl contains special characters, it should be encoded before
+   * passed to this constructor.
+   */
+  private val baseUrl: String,
+) : UrlBuilder {
   override fun build(path: String): String {
     val base = removeSuffix(baseUrl, "/")
     return if (path.startsWith("/")) "${base}${path}" else path
