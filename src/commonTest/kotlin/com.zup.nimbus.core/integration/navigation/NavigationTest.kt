6ba84fadd9f91998d0f9e1c6e6ae21d4e0b80fa0
Tiago Peres França <tiago.franca@zup.com.br>
2022-05-04 09:40:44 -0300

    better test code

--- src/commonTest/kotlin/com.zup.nimbus.core/integration/navigation/NavigationTest.kt
+++ src/commonTest/kotlin/com.zup.nimbus.core/integration/navigation/NavigationTest.kt
@@ -5,6 +5,7 @@ import com.zup.nimbus.core.ServerDrivenConfig
 import com.zup.nimbus.core.network.DefaultHttpClient
 import com.zup.nimbus.core.network.ResponseError
 import com.zup.nimbus.core.network.ViewRequest
+import com.zup.nimbus.core.tree.ServerDrivenNode
 import io.ktor.http.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestScope
@@ -23,171 +24,137 @@ class NavigationTest {
     httpClient = DefaultHttpClient(serverMock),
   ))
 
-  @Test
-  fun shouldRenderFirstView() = scope.runTest {
+  private fun runNavigationTest(
+    firstScreen: String = "/screen1",
+    onLoad: (pages: ArrayList<Page>) -> Unit = { fail("An error was expected.") },
+    onError: ((e: Error) -> Unit) = { fail("Unexpected error: ${it.message}") },
+  ) {
     val navigator = Navigator(
       scope = scope,
       nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        assertEquals(1, it.size)
-        verifyScreen1(it.last().content)
-      },
+      onError = onError,
+      onLoad = onLoad,
     )
-    navigator.push(ViewRequest("/screen1"))
+    navigator.push(ViewRequest(firstScreen))
+  }
+
+  private fun pressNextButton(pageContent: ServerDrivenNode?, buttonIndex: Int = 1) {
+    val nextButton = pageContent?.children?.get(buttonIndex)
+    val onPress = nextButton?.properties?.get("onPress")
+    if (onPress is Function0<*>) onPress.invoke()
+  }
+
+  private fun pressPreviousButton(pageContent: ServerDrivenNode?) {
+    val previousButton = pageContent?.children?.last()
+    val onPress = previousButton?.properties?.get("onPress")
+    if (onPress is Function0<*>) onPress.invoke()
+  }
+
+  @Test
+  fun shouldRenderFirstView() = scope.runTest {
+    runNavigationTest(onLoad = {
+      assertEquals(1, it.size)
+      verifyScreen1(it.last().content)
+    })
   }
 
   @Test
   fun shouldNotFindFirstViewWithWrongUrl() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
+    runNavigationTest(
+      firstScreen = "/none",
       onError = {
         assertEquals(true, it is ResponseError)
         it as ResponseError
         assertEquals(HttpStatusCode.NotFound.value, it.status)
       },
-      onLoad = { fail("An error was expected.") },
     )
-    navigator.push(ViewRequest("/none"))
   }
 
   @Test
   fun shouldPushSecondView() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        if(it.last().id == "/screen1") {
-          val nextButton = it.last().content?.children?.get(1)
-          val onPress = nextButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-        } else {
-          assertEquals(2, it.size)
-          verifyScreen2(it.last().content)
-        }
-      },
-    )
-    navigator.push(ViewRequest("/screen1"))
+    runNavigationTest(onLoad = {
+      if(it.last().id == "/screen1") {
+        pressNextButton(it.last().content)
+      } else {
+        assertEquals(2, it.size)
+        verifyScreen2(it.last().content)
+      }
+    })
   }
 
   @Test
   fun shouldPushThirdView() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        if(it.last().id == "/screen1" || it.last().id == "/screen2") {
-          val nextButton = it.last().content?.children?.get(1)
-          val onPress = nextButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-        } else {
-          assertEquals(3, it.size)
-          verifyScreen3(it.last().content)
-        }
-      },
-    )
-    navigator.push(ViewRequest("/screen1"))
+    runNavigationTest(onLoad = {
+      if(it.last().id == "/screen1" || it.last().id == "/screen2") {
+        pressNextButton(it.last().content)
+      } else {
+        assertEquals(3, it.size)
+        verifyScreen3(it.last().content)
+      }
+    })
   }
 
   @Test
   fun shouldShowFallbackWhenPushingFourthView() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        if(it.last().id == "/screen1" || it.last().id == "/screen2" || it.last().id == "/screen3") {
-          val nextButton = it.last().content?.children?.get(1)
-          val onPress = nextButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-        } else {
-          assertEquals(4, it.size)
-          verifyFallbackScreen(it.last().content)
-        }
-      },
-    )
-    navigator.push(ViewRequest("/screen1"))
+    runNavigationTest(onLoad = {
+      if(it.last().id == "/screen1" || it.last().id == "/screen2" || it.last().id == "/screen3") {
+        pressNextButton(it.last().content)
+      } else {
+        assertEquals(4, it.size)
+        verifyFallbackScreen(it.last().content)
+      }
+    })
   }
 
   @Test
   fun shouldProduceErrorAndNotNavigateWhenGoingToScreen4() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
+    runNavigationTest(
+      onLoad = {
+        if (it.last().id == "/screen4") fail("we didn't expect to able to load /screen4.")
+        // for "/screen3", there will be two next buttons, we want to press the second
+        val nextButtonIndex = if (it.last().id == "/screen3") 2 else 1
+        pressNextButton(it.last().content, nextButtonIndex)
+      },
       onError = {
         assertEquals(true, it is ResponseError)
         it as ResponseError
         assertEquals(404, it.status)
-      },
-      onLoad = {
-        val index = if (it.last().id == "/screen3") 2 else 1
-        val nextButton = it.last().content?.children?.get(index)
-        val onPress = nextButton?.properties?.get("onPress")
-        if (onPress is Function0<*>) onPress.invoke()
-      },
+      }
     )
-    navigator.push(ViewRequest("/screen1"))
   }
 
   @Test
   fun shouldPushSecondViewAndPop() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        if(it.last().id == "/screen1") {
-          val nextButton = it.last().content?.children?.get(1)
-          val onPress = nextButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-        } else {
-          val previousButton = it.last().content?.children?.get(2)
-          val onPress = previousButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-          assertEquals(true, it.size == 1)
-          assertEquals("/screen1", it.last().id)
-        }
-      },
-    )
-    navigator.push(ViewRequest("/screen1"))
+    runNavigationTest(onLoad = {
+      if(it.last().id == "/screen1") {
+        pressNextButton(it.last().content)
+      } else {
+        pressPreviousButton(it.last().content)
+        assertEquals(true, it.size == 1)
+        assertEquals("/screen1", it.last().id)
+      }
+    })
   }
 
   @Test
   fun shouldPopToRootFromFallback() = scope.runTest {
-    val navigator = Navigator(
-      scope = scope,
-      nimbus = nimbus,
-      onError = { fail("Unexpected error: ${it.message}") },
-      onLoad = {
-        if(it.last().id == "/screen1" || it.last().id == "/screen2" || it.last().id == "/screen3") {
-          val nextButton = it.last().content?.children?.get(1)
-          val onPress = nextButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-        } else {
-          // fallback to /screen3
-          var previousButton = it.last().content?.children?.get(1)
-          var onPress = previousButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-          assertEquals(true, it.size == 3)
-          assertEquals("/screen3", it.last().id)
-          // /screen3 to /screen2
-          previousButton = it.last().content?.children?.get(3)
-          onPress = previousButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-          assertEquals(true, it.size == 2)
-          assertEquals("/screen2", it.last().id)
-          // /screen2 to /screen1
-          previousButton = it.last().content?.children?.get(2)
-          onPress = previousButton?.properties?.get("onPress")
-          if (onPress is Function0<*>) onPress.invoke()
-          assertEquals(true, it.size == 1)
-          assertEquals("/screen1", it.last().id)
-        }
-      },
-    )
-    navigator.push(ViewRequest("/screen1"))
+    runNavigationTest(onLoad = {
+      if(it.last().id == "/screen1" || it.last().id == "/screen2" || it.last().id == "/screen3") {
+        pressNextButton(it.last().content)
+      } else {
+        // fallback to /screen3
+        pressPreviousButton(it.last().content)
+        assertEquals(true, it.size == 3)
+        assertEquals("/screen3", it.last().id)
+        // /screen3 to /screen2
+        pressPreviousButton(it.last().content)
+        assertEquals(true, it.size == 2)
+        assertEquals("/screen2", it.last().id)
+        // /screen2 to /screen1
+        pressPreviousButton(it.last().content)
+        assertEquals(true, it.size == 1)
+        assertEquals("/screen1", it.last().id)
+      }
+    })
   }
 }
