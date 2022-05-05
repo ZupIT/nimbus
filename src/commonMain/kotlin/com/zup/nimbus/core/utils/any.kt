c38d0039933baec6b547782373faff95cbeb1737
Tiago Peres França <tiago.franca@zup.com.br>
2022-05-05 15:04:42 -0300

    kdocs for valueOf

--- src/commonMain/kotlin/com/zup/nimbus/core/utils/any.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/utils/any.kt
@@ -35,6 +35,35 @@ fun extractValueOfMap(data: Any, accessor: String): Any? {
   return if (data is Map<*, *>) data[key] else null
 }
 
+/**
+ * Returns `data` at `path` casted to the expected type (T).
+ *
+ * If `path` is empty and `data` can be casted to T, `data` is returned. If `data` can't be casted to T,
+ * UnexpectedDataTypeError is thrown.
+ *
+ * `path` specifies which property of the data structure we need. If `data` is a primitive type and `path` is not empty,
+ * null is returned, because we can't get a path from a primitive type. If `data` is a map and `path` is "foo", the
+ * value of the key "foo" is returned. If `data` is a list and `path` is `"[0]"`, the first position of the array is
+ * returned.
+ *
+ * The path can be composed of keys (.key) and indexes (`[index]`). Example: `"foo.bar[0][1].text"`. In this example,
+ * `data` must be a map with the key "foo", which must be a map with the key "bar", which must be a list where the
+ * first position is another list where the second position is a map. The property "text" of this last map is casted to
+ * T and returned.
+ *
+ * If `path` doesn't exist in `data` and T is nullable, null is returned. If T is not nullable, UnexpectedDataTypeError
+ * is thrown.
+ *
+ * This function is not responsible for validating the path, but if it does encounter a problem, InvalidDataPathError
+ * is thrown.
+ *
+ * @param data the data source to get the return value from.
+ * @param path the path to the item we want in `data`.
+ * @throws InvalidDataPathError if an error is encountered while evaluating the path string.
+ * @throws UnexpectedDataTypeError if the type of the encountered result (or null if no result is encountered) is not
+ * the same as the expected type (T).
+ */
+@Throws(InvalidDataPathError::class, UnexpectedDataTypeError::class)
 inline fun <reified T>valueOf(data: Any?, path: String = ""): T {
   var current: Any? = data
   val accessors = dataPathRegex.findAll(path).iterator()
