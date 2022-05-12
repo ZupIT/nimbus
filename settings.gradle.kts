0a3078ab676b38bba2202619dfb0de00dbd4b043
Tiago Peres França <tiago.franca@zup.com.br>
2022-05-12 14:38:11 -0300

    build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,15 @@ pluginManagement {
   }
 }
 
+plugins {
+  id("com.gradle.enterprise") version("3.10")
+}
+
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
+
 rootProject.name = "nimbus-core"
