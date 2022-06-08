685edc8ffa998994ee3e180ce29dd37950e2ee03
Arthur Bleil <arthur.bleil@zup.com.br>
2022-06-08 17:00:43 -0300

    fix linter 2
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- src/commonMain/kotlin/com/zup/nimbus/core/operations/insert.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/operations/insert.kt
@@ -2,6 +2,9 @@ package com.zup.nimbus.core.operations
 
 import com.zup.nimbus.core.OperationHandler
 
+private const val minimumArraySize = 3
+private const val indexPosition = 2
+
 private fun insertOperation(array: Array<Any>, item: Any, index: Int?): MutableList<Any> {
   val list = array.toMutableList()
   if (index != null) list.add(index, item) else list.add(item)
@@ -9,6 +12,6 @@ private fun insertOperation(array: Array<Any>, item: Any, index: Int?): MutableL
 }
 
 val insert: OperationHandler = {
-  val index = if (it.size >= 3) (it[2] as Number).toInt() else null
+  val index = if (it.size >= minimumArraySize) (it[indexPosition] as Number).toInt() else null
   insertOperation(it[0] as Array<Any>, it[1] as Any, index)
 }
