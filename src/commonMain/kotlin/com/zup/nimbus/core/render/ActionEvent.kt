ac18682c7d235dcc8bba4bd7e982b41744a5b488
Arthur Bleil <arthur.bleil@zup.com.br>
2022-07-22 16:31:01 -0300

    enhancement: return the new value on the observable state listener
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- src/commonMain/kotlin/com/zup/nimbus/core/render/ActionEvent.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/render/ActionEvent.kt
@@ -23,3 +23,4 @@ data class ActionEvent(
    */
   val view: ServerDrivenView,
 )
+
--- src/commonMain/kotlin/com/zup/nimbus/core/tree/ObservableState.kt
+++ src/commonMain/kotlin/com/zup/nimbus/core/tree/ObservableState.kt
@@ -1,7 +1,10 @@
 package com.zup.nimbus.core.tree
 
+typealias ObservableStateListener = (value: Any?) -> Unit
+typealias RemovableObservableStateListener = () -> Unit
+
 class ObservableState(id: String, value: Any?): ServerDrivenState(id, value, null) {
-  private val listeners = ArrayList<() -> Unit>()
+  private val listeners = ArrayList<ObservableStateListener>()
 
   /**
    * Listens to changes to this state's value.
@@ -9,20 +12,14 @@ class ObservableState(id: String, value: Any?): ServerDrivenState(id, value, nul
    * @param listener the function to run when the value of this state changes.
    * @return a function to remove this listener.
    */
-  fun onChange(listener: () -> Unit): (() -> Unit) {
+  fun onChange(listener: ObservableStateListener): RemovableObservableStateListener {
     listeners.add(listener)
     return { listeners.remove(listener) }
   }
 
   override fun set(newValue: Any?, path: String) {
-    // if the new value is a map or list, it must be mutable.
-    var mutableValue = newValue
-    if (newValue is Map<*, *> && newValue !is MutableMap<*, *>) mutableValue = newValue.toMutableMap()
-    else if (newValue is List<*> && newValue !is MutableList<*>) mutableValue = newValue.toMutableList()
-    // super
-    super.set(mutableValue, path)
-    // notify
-    listeners.forEach { it() }
+    super.set(newValue, path)
+    listeners.forEach { it(super.value) }
   }
 
   fun set(newValue: Any?) {
