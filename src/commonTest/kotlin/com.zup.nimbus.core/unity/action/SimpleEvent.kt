cd99a4b911baf10f6db8628e924704d3576a1b8e
Tiago Peres França <tiago.franca@zup.com.br>
2022-09-21 09:16:11 -0300

    fixes tests

--- src/commonTest/kotlin/com.zup.nimbus.core/unity/action/SimpleEvent.kt
+++ src/commonTest/kotlin/com.zup.nimbus.core/unity/action/SimpleEvent.kt
@@ -7,6 +7,7 @@ import com.zup.nimbus.core.dependency.CommonDependency
 import com.zup.nimbus.core.scope.CommonScope
 import com.zup.nimbus.core.scope.Scope
 import com.zup.nimbus.core.scope.closestScopeWithType
+import com.zup.nimbus.core.tree.ServerDrivenAction
 import com.zup.nimbus.core.tree.ServerDrivenEvent
 import com.zup.nimbus.core.tree.ServerDrivenNode
 
@@ -16,12 +17,13 @@ class SimpleEvent(
   states: List<ServerDrivenState>? = null,
 ): ServerDrivenEvent, CommonDependency(), Scope by CommonScope(states, parent) {
   var calls: MutableList<Any?> = mutableListOf()
+  override val actions: List<ServerDrivenAction> = emptyList()
   override val node: ServerDrivenNode
     get() = throw NotImplementedError()
   override val view: ServerDrivenView
     get() = throw NotImplementedError()
   override val nimbus: Nimbus
-    get() = closestScopeWithType() ?: throw Error("Nimbus is not available")
+    get() = closestScopeWithType() ?: throw IllegalStateException("Nimbus is not available")
 
   override fun run() {
     calls.add(null)
