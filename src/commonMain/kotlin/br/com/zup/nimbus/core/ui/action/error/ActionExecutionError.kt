538f648a887e69e50314639e15bc1390becac4a0
Tiago Peres França <tiago.franca@zup.com.br>
2022-11-30 11:43:26 -0300

    makes error messages for actions accessible from iOS

--- src/commonMain/kotlin/br/com/zup/nimbus/core/ui/action/error/ActionExecutionError.kt
+++ src/commonMain/kotlin/br/com/zup/nimbus/core/ui/action/error/ActionExecutionError.kt
@@ -3,7 +3,19 @@ package br.com.zup.nimbus.core.ui.action.error
 import br.com.zup.nimbus.core.ActionEvent
 import br.com.zup.nimbus.core.scope.getPathToScope
 
-class ActionExecutionError(event: ActionEvent, override val cause: Throwable): Error() {
+class ActionExecutionError private constructor (
+  event: ActionEvent,
+  cause: Throwable?,
+  message: String,
+): Error(cause = cause) {
+  constructor(event: ActionEvent, cause: Throwable): this(
+    event,
+    cause,
+    cause.message ?: cause::class.qualifiedName ?: "unknown",
+  )
+
+  constructor(event: ActionEvent, message: String): this(event, null, message)
+
   override val message: String = "Error while running the action \"${event.action.name}\".\nAt: " +
-    "${event.scope.getPathToScope()}\nCaused by: ${cause.message ?: cause::class.qualifiedName}"
+    "${event.scope.getPathToScope()}\nCaused by: $message"
 }
