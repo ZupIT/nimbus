f2fb0f1cc93cadcb32dbcd540aad356641ce2f82
Tiago Peres França <tiago.franca@zup.com.br>
2023-01-06 10:29:31 -0300

    creates new performance test (#74)

--- src/commonTest/kotlin/br/com/zup/nimbus/core/performance/PerformanceTest.kt
+++ src/commonTest/kotlin/br/com/zup/nimbus/core/performance/PerformanceTest.kt
@@ -21,7 +21,9 @@ import br.com.zup.nimbus.core.JsonLoader
 import br.com.zup.nimbus.core.Nimbus
 import br.com.zup.nimbus.core.NodeUtils
 import br.com.zup.nimbus.core.ServerDrivenConfig
+import br.com.zup.nimbus.core.dependency.Dependent
 import br.com.zup.nimbus.core.tree.ServerDrivenNode
+import br.com.zup.nimbus.core.tree.dynamic.node.DynamicNode
 import br.com.zup.nimbus.core.tree.findNodeById
 import br.com.zup.nimbus.core.ui.UILibrary
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -36,6 +38,7 @@ import kotlin.test.assertTrue
 
 private const val MAX_AVERAGE_UPDATE_TIME_MS = 15
 private const val FOR_EACH_MAX_AVERAGE_UPDATE_TIME_MS = 15
+private const val MAX_TIME_FOR_FILTERING_LIST_MS = 10
 private const val SHOULD_PRINT_TIMES = false
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -103,6 +106,43 @@ class PerformanceTest {
   @Test
   fun `should add to cart in reasonable time - with forEach`() = scope.runTest {
     runPerformanceTest("products-forEach", FOR_EACH_MAX_AVERAGE_UPDATE_TIME_MS)
+  }
+
+  private fun onUpdateEachNode(node: DynamicNode, listener: (ServerDrivenNode) -> Unit) {
+    node.addDependent(object: Dependent {
+      override fun update() {
+        listener(node)
+      }
+    })
+    node.children?.forEach { onUpdateEachNode(it, listener) }
+  }
+
+  @Test
+  fun `should filter for each list without performance hiccups`() {
+    // WHEN the "to-do" screen is rendered
+    val json = JsonLoader.loadJson("to-do")
+    val nimbus = Nimbus(ServerDrivenConfig("", "test", httpClient = EmptyHttpClient))
+    val tree = nimbus.nodeBuilder.buildFromJsonString(json)
+    tree.initialize(nimbus)
+    // AND we start measuring time
+    val start = Clock.System.now().toEpochMilliseconds()
+    // AND we filter the notes to show only the "to do" notes
+    NodeUtils.triggerEvent(tree.findNodeById("status-filter"), "onChange", "To do")
+    // THEN it shouldn't have taken to much time to render the filtered list
+    val end = Clock.System.now().toEpochMilliseconds() - start
+    if (SHOULD_PRINT_TIMES) println("===> ELAPSED TIME: ${end}ms") // around 5ms on both Android and iOS
+    assertTrue(end < MAX_TIME_FOR_FILTERING_LIST_MS)
+    // WHEN we remove the filter making the list go back to its original state
+    NodeUtils.triggerEvent(tree.findNodeById("status-filter"), "onChange", "All")
+    // AND we start counting every node update
+    val updates = mutableListOf<String>()
+    onUpdateEachNode(tree) { updates.add(it.component) }
+    // AND we filter the notes to show only the "to do"
+    NodeUtils.triggerEvent(tree.findNodeById("status-filter"), "onChange", "To do")
+    // THEN it should have updated only two components: the filter itself (selection group) and the scroll view
+    assertEquals(2, updates.size)
+    assertTrue(updates.contains("todoapp:selectionGroup"))
+    assertTrue(updates.contains("layout:scrollView"))
   }
 }
 
